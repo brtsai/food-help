43fa08ec6bd5ad5fc946388d6dd9a207d73175e6
brtsai <bryan@bryantsaiwh.com>
2018-04-03 15:42:34 -0700

    implement backend route, controller for creating a new user

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
+    resources :users, only: [:create]
   end
 end
