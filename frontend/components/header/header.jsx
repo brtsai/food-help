08d9f47a3042bc989d16c7b042df71b6d590081f
brtsai <bryan@bryantsaiwh.com>
2018-04-05 11:36:31 -0700

    switch logo src back to assets/logo.png

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
-    return <nav className="header-nav"><Link to="/"><img className="header-nav-logo" src="https://github.com/brtsai/food-help/blob/master/app/assets/images/logo.png"></img></Link></nav>;
+    return <nav className="header-nav"><Link to="/"><img className="header-nav-logo" src="/assets/logo.png"></img></Link></nav>;
   }
 }
 
