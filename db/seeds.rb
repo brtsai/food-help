e8007f025569219db7d82b4d1babad3f9b7b21a9
brtsai <bryan@bryantsaiwh.com>
2018-04-05 14:45:51 -0700

    added guest user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+demoLogin = User.create({ username: 'guest', password: 'password', city: 'San Francisco', state: 'California' })
