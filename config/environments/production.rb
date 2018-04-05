28cd7af157e58ea5c269aef389551cee721bf0c7
brtsai <bryan@bryantsaiwh.com>
2018-04-05 11:38:49 -0700

    switch production.rb config file to serve static assets and fallback on the assetes pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  config.assets.compile = true
+  # serve my static assets
+  config.serve_static_assets = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
