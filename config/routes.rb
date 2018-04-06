7220aa4d6bd02241b93c9cb3027d3ca2fe4aaca1
brtsai <bryan@bryantsaiwh.com>
2018-04-05 17:03:00 -0700

    add routes for api/business endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
     resources :users, only: [:create]
+    resources :businesses, only: [:index, :show, :create]
   end
 
   root "static_pages#root"
