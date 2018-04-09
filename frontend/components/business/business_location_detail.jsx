67ac3a0fb3f6c9982c67a49862ebc63ae0b31387
brtsai <bryan@bryantsaiwh.com>
2018-04-09 11:15:59 -0700

    fix static map image marker not showing up

--- frontend/components/business/business_location_detail.jsx
+++ frontend/components/business/business_location_detail.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class BusinessLocationDetail extends React.Component {
   staticMapImageString() {
-    return "https://maps.googleapis.com/maps/api/staticmap?scale=2&center=" + this.props.business.latitude + "," + this.props.business.longitude + "8&zoom=15&size=286x135&markers=color:red%7C%7C37.7809504,-122.4067438&key=AIzaSyBPzCpqLjnn8cBlaCMdz1v0mlRMtlnW4Ro"
+    return "https://maps.googleapis.com/maps/api/staticmap?scale=2&center=" + this.props.business.latitude + "," + this.props.business.longitude + "8&zoom=15&size=286x135&markers=color:red%7C%7C" + this.props.business.latitude + "," + this.props.business.longitude + "8&key=AIzaSyBPzCpqLjnn8cBlaCMdz1v0mlRMtlnW4Ro"
   }
 
   render () {
