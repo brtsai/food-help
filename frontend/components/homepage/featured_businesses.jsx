34cadf2591499f99a051a81aa71d662df25d082d
brtsai <bryan@bryantsaiwh.com>
2018-04-10 16:50:28 -0700

    add featured businesses placeholder component, style homepage more

---
+++ frontend/components/homepage/featured_businesses.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class FeaturedBusinesses extends React.Component {
+  render () {
+    return (
+      <section className="featured-businesses-section">
+        Featured Businesses Component
+      </section>
+    );
+  }
+}
+
+export default FeaturedBusinesses;
--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SplashSearchContainer from './splash_search_container';
+import FeaturedBusinesses from './featured_businesses';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className="homepage-component">
         <SplashSearchContainer />
+        <FeaturedBusinesses />
       </div>
     );
   }
