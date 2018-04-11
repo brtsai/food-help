d420f09e025636b93ef8a1fc396ac405ae8736e0
brtsai <bryan@bryantsaiwh.com>
2018-04-11 14:25:54 -0700

    add 5 stock business profile images

--- frontend/components/search/search_result_item.jsx
+++ frontend/components/search/search_result_item.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 class SearchResultItem extends React.Component {
+  mapIdToStockImageUrl (id) {
+    const imageUrls = [
+      "https://scontent.cdninstagram.com/vp/42b4c0778a3daf9ac17cc8aecb2906ef/5B5BEE3E/t51.2885-15/sh0.08/e35/p640x640/26870995_737504946443421_4986542655124013056_n.jpg",
+      "https://scontent.cdninstagram.com/vp/c54dd5aca82a7073e46c1e036eccb7d4/5B55C1A9/t51.2885-15/s640x640/sh0.08/e35/28152121_165953720865739_1522247889873534976_n.jpg",
+      "https://scontent.cdninstagram.com/vp/b6139e2f614810312507340083100acb/5B675980/t51.2885-15/s640x640/sh0.08/e35/27892505_422662831479780_1102797140318486528_n.jpg",
+      "https://scontent.cdninstagram.com/vp/a6345337e3f8717ccb1673bf61dbde8b/5AD0B5EE/t51.2885-15/s320x320/e35/29417525_585293405167767_787768809644621824_n.jpg",
+      "https://scontent.cdninstagram.com/vp/1a1551ca874fbb897c50721bb1ce9ebd/5B55ED66/t51.2885-15/e35/p320x320/27576408_191774751577893_1680923175996620800_n.jpg"
+    ];
+
+    return imageUrls[id%5];
+  }
+
   priceDollarSigns() {
     let dollars = "";
     for(let i = 0; i < this.props.business.price; i++) {
@@ -34,7 +46,7 @@ class SearchResultItem extends React.Component {
   render () {
     return (
       <li className="search-result-item">
-        <Link to={ `/biz/${this.props.business.id}` }><img className="search-result-item-image" src="https://s3-media3.fl.yelpcdn.com/assets/srv0/yelp_styleguide/bed0b2a1d9e2/assets/img/default_avatars/business_60_square.png"></img></Link>
+        <Link to={ `/biz/${this.props.business.id}` }><img className="search-result-item-image" src= { this.mapIdToStockImageUrl(this.props.business.id)} ></img></Link>
 
         <div className="search-result-text-container">
           
