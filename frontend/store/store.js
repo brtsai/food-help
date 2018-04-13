987159bd2404fbd814f7619b678efe2abeb9fd27
brtsai <bryan@bryantsaiwh.com>
2018-04-13 11:27:37 -0700

    conditionally have logger as a middleware in store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,11 +4,19 @@ import logger from 'redux-logger';
 
 import RootReducer from '../reducers/root_reducer'
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer, 
     preloadedState, 
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 );
 
