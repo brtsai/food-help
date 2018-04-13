2f758d291c967c6a17a803bcc64f4b374b432069
brtsai <bryan@bryantsaiwh.com>
2018-04-13 11:34:43 -0700

    removed unconditional require of redux logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-
 import RootReducer from '../reducers/root_reducer'
 
 const middlewares = [thunk];
