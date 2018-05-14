83f2e8b9a7aa1db82dc1c30ab7bcfd9554e07d7d
brtsai <bryan@bryantsaiwh.com>
2018-05-14 16:06:01 -0700

    remove debounced on location field

--- frontend/components/business/create/add_business_form.jsx
+++ frontend/components/business/create/add_business_form.jsx
@@ -115,7 +115,7 @@ class AddBusinessForm extends React.Component {
         </label>
 
         <label> Location
-          <input type="string" onChange= { this.updateLocation } value={this.state.businessLocation} />
+          <input type="string" onChange= { this.update('businessLocation') } value={this.state.businessLocation} />
         </label>
 
         <label> Price
