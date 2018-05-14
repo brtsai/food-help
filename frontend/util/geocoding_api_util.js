ae89afe849f94b349126362fc183ed5d42d9cbfb
brtsai <bryan@bryantsaiwh.com>
2018-05-14 15:32:14 -0700

    fix typo in geocoding api util

--- frontend/util/geocoding_api_util.js
+++ frontend/util/geocoding_api_util.js
@@ -1,8 +1,8 @@
 export const geocode = place => (
   $.ajax({
     method: 'GET',
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${
-      encodeURIComponent(address)
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${
+      encodeURIComponent(place)
     }&key=AIzaSyADVVFN-sWPtq7Lqp7nvs_mUYm1nL1Nj1o`
   })
 );
