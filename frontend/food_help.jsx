e5c73179fd21088cff340ff97bc9331ca960484c
brtsai <bryan@bryantsaiwh.com>
2018-05-14 15:33:12 -0700

    remove geocoding api util from browser

--- frontend/food_help.jsx
+++ frontend/food_help.jsx
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store.js';
 
-import * as GeocodingAPIUtil from './util/geocoding_api_util';
-
 document.addEventListener('DOMContentLoaded', () => {
 
   const root = document.getElementById('root');
@@ -18,7 +16,5 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore();
   }
   
-  window.GeocodingAPIUtil = GeocodingAPIUtil;
-
   ReactDOM.render(<Root store={store}/>, root);
 });
