eb880ee3c90c3d0243e78a335d3049e6c822a142
brtsai <bryan@bryantsaiwh.com>
2018-05-15 16:38:04 -0700

    add ui to root redux reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 import entitiesReducer from './entities_reducer';
+import uiReducer from './ui_reducer';
 
 export default combineReducers({
   entities: entitiesReducer,
   session: sessionReducer,
-  errors: errorsReducer
+  errors: errorsReducer,
+  ui: uiReducer
 });
