32fb42fdcf26c78ffed5ff0d0721f7d014485c04
brtsai <bryan@bryantsaiwh.com>
2018-05-15 16:54:50 -0700

    add stub for parsing geocoding

--- frontend/reducers/geocoding_reducer.js
+++ frontend/reducers/geocoding_reducer.js
@@ -2,14 +2,25 @@ import { merge } from 'lodash';
 
 import { RECEIVE_GEOCODING } from '../actions/geocoding_actions';
 
+const convertGeocoding = geocoding => {
+  console.log('attempting conversion');
+  console.log(geocoding);
+
+  return {
+
+  };
+}
+
 const geocodingReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch (action.type) {
     case RECEIVE_GEOCODING:
       console.log('received geocoding');
-      console.log(action.geocoding);
-      return state;
+      cosnole.log(action.geocoding);
+      const converted = convertGeocoding(action.geocoding);
+      console.log(converted);
+      return converted;
 
     default:
       return state;
