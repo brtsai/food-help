979d1e600a6fa0001de9246d83c8423fd8622cba
brtsai <bryan@bryantsaiwh.com>
2018-05-15 16:47:37 -0700

    add geocoding errors reducer skeleton

---
+++ frontend/reducers/geocoding_errors_reducer.js
@@ -0,0 +1,9 @@
+import { merge } from 'lodash';
+
+import { RECEIVE_GEOCODING_ERRORS } from '../actions/business_actions';
+
+const geocodingErrorsReducer = (state = [], action) => {
+  return state
+};
+
+export default geocodingErrorsReducer;
