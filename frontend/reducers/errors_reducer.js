23a10687d4c6eda72cf30a06fa42084f92303a56
brtsai <bryan@bryantsaiwh.com>
2018-05-15 16:48:19 -0700

    add geocoding errors to errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -3,10 +3,12 @@ import sessionErrorsReducer from './session_errors_reducer';
 import businessErrorsReducer from './business_errors_reducer';
 import searchErrorsReducer from './search_errors_reducer';
 import reviewErrorsReducer from './review_errors_reducer';
+import geocodingErrorsReducer from './geocoding_errors_reducer';
 
 export default combineReducers({ 
   login: sessionErrorsReducer,
   business: businessErrorsReducer,
   search: searchErrorsReducer,
-  review: reviewErrorsReducer
+  review: reviewErrorsReducer,
+  geocoding: geocodingErrorsReducer
 });
