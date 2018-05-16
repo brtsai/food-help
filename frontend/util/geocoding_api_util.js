01f8539aa6bab37ee6de273966b9bab78a296a7d
brtsai <bryan@bryantsaiwh.com>
2018-05-15 17:08:51 -0700

    restrict geocoding to USA

--- frontend/util/geocoding_api_util.js
+++ frontend/util/geocoding_api_util.js
@@ -3,6 +3,6 @@ export const geocode = place => (
     method: 'GET',
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${
       encodeURIComponent(place)
-    }&key=AIzaSyADVVFN-sWPtq7Lqp7nvs_mUYm1nL1Nj1o`
+    }&components=country:US&key=AIzaSyADVVFN-sWPtq7Lqp7nvs_mUYm1nL1Nj1o`
   })
 );
