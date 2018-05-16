6919650177e32ba40b230a25194614d8d0698bd4
brtsai <bryan@bryantsaiwh.com>
2018-05-15 17:19:30 -0700

    remove console logs

--- frontend/reducers/geocoding_reducer.js
+++ frontend/reducers/geocoding_reducer.js
@@ -3,17 +3,16 @@ import { merge } from 'lodash';
 import { RECEIVE_GEOCODING } from '../actions/geocoding_actions';
 
 const convertGeocoding = geocoding => {
-  console.log('attempting conversion');
-  console.log(geocoding);
   const formattedAddress = geocoding.formatted_address;
   const split = formattedAddress.split(',').map(component => component.trim());
-  console.log(split);
   const address = split[0];
   const city = split[1];
+  const state = split[2].split(' ')[0];
 
   return {
     address,
     city,
+    state,
     lat: geocoding.geometry.location.lat,
     lng: geocoding.geometry.location.lng
   };
@@ -24,10 +23,7 @@ const geocodingReducer = (state = {}, action) => {
 
   switch (action.type) {
     case RECEIVE_GEOCODING:
-      console.log('received geocoding');
-      console.log(action.geocoding);
       const converted = convertGeocoding(action.geocoding);
-      console.log(converted);
       return converted;
 
     default:
