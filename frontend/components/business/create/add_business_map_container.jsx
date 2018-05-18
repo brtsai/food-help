835fce77630f6033fc30066e29bc8fdd57e45586
brtsai <bryan@bryantsaiwh.com>
2018-05-17 18:34:47 -0700

    wrap add business map in redux container

---
+++ frontend/components/business/create/add_business_map_container.jsx
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux'
+
+import AddBusinessMap from './add_business_map.jsx';
+
+const mapStateToProps = state => ({
+  
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddBusinessMap);
