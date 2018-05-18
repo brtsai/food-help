e8f77bb0f08286819f068d81637f17ab078fd377
brtsai <bryan@bryantsaiwh.com>
2018-05-17 18:31:49 -0700

    create gmap component for add business page

---
+++ frontend/components/business/create/add_business_map.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class AddBusinessMap extends React.Component {
+  componentDidMount() {
+    //const latitude = parseFloat(this.props.business.latitude);
+    //const longitude = parseFloat(this.props.business.longitude);
+    // set the map to show SF
+    const latitude = 37.3318;
+    const longitude = -122.0312;
+    const mapOptions = {
+      center: { lat: latitude, lng: longitude }, // this is SF
+      zoom: 13
+    };
+
+    // wrap the mapDOMNode in a Google Map
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+    
+    const marker = new google.maps.Marker({
+      position: { lat: latitude, lng: longitude },
+      map: this.map
+    });
+  }
+
+  render () {
+    return (
+      <div className="add-business-google-map-container">
+        <div className="google-map" ref={ map => this.mapNode = map }></div>
+      </div>
+
+    );
+  }
+
+}
+
+export default AddBusinessMap;
