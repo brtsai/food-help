6ca4793260996846479f17e6434940d26d93cc5f
brtsai <bryan@bryantsaiwh.com>
2018-05-18 18:49:17 -0700

    map ui lat lng to add business map

--- frontend/components/business/create/add_business_map_container.jsx
+++ frontend/components/business/create/add_business_map_container.jsx
@@ -3,7 +3,8 @@ import { connect } from 'react-redux'
 import AddBusinessMap from './add_business_map.jsx';
 
 const mapStateToProps = state => ({
-  
+  lat: state.ui.geocoding.lat,
+  lng: state.ui.geocoding.lng
 });
 
 const mapDispatchToProps = dispatch => ({
