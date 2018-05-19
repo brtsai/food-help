ec362ba1c5f5b732929fba275545254ddd5adb44
brtsai <bryan@bryantsaiwh.com>
2018-05-19 14:33:24 -0700

    implement geocoding errors reducer

--- frontend/reducers/geocoding_errors_reducer.js
+++ frontend/reducers/geocoding_errors_reducer.js
@@ -3,7 +3,13 @@ import { merge } from 'lodash';
 import { RECEIVE_GEOCODING_ERRORS } from '../actions/geocoding_actions';
 
 const geocodingErrorsReducer = (state = [], action) => {
-  return state
+  switch (action.type) {
+    case RECEIVE_GEOCODING_ERRORS:
+      return action.errors;
+    
+    default:
+      return [];
+  }
 };
 
 export default geocodingErrorsReducer;
