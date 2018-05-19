2e0f4b3189e2d9be29550453ba0f799e3892af38
brtsai <bryan@bryantsaiwh.com>
2018-05-19 16:36:18 -0700

    map geocode dispatch to add biz form props

--- frontend/components/business/create/add_business_form_container.jsx
+++ frontend/components/business/create/add_business_form_container.jsx
@@ -3,13 +3,15 @@ import { withRouter } from 'react-router-dom';
 
 import AddBusinessForm from './add_business_form';
 import { addBusiness } from '../../../actions/business_actions'
+import { geocodeLocation } from '../../../actions/geocoding_actions';
 
 const mapStateToProps = state => ({
   ownerId: Object.keys(state.session)[0]
 });
 
 const mapDispatchToProps = dispatch => ({
-  addBusiness: business => dispatch(addBusiness(business))
+  addBusiness: business => dispatch(addBusiness(business)),
+  geocodeLocation: place => dispatch(geocodeLocation(place))
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AddBusinessForm));
