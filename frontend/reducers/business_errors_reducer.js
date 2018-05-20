18d6fd87d7520424f38c98d1d68c44d094ee7f73
brtsai <bryan@bryantsaiwh.com>
2018-05-20 01:12:37 -0700

    map location related errors to Invalid Location error

--- frontend/reducers/business_errors_reducer.js
+++ frontend/reducers/business_errors_reducer.js
@@ -3,13 +3,40 @@ import { RECEIVE_ROUTE_CHANGE } from '../actions/app_actions';
 import { RECEIVE_GEOCODING_ERRORS } from '../actions/geocoding_actions';
 import { merge } from 'lodash';
 
+const convertLocationErrors = errors => {
+  const newErrors = [];
+  const locationErrors = {
+    "Address can't be blank": true,
+    "City can't be blank": true,
+    "State can't be blank": true,
+    "Latitude can't be blank": true,
+    "Longitude can't be blank": true,
+  };
+  let locationErrorPresent = false;
+
+  errors.forEach(error => {
+    if (locationErrors[error]) {
+      locationErrorPresent = true;
+    } else {
+      newErrors.push(error);
+    }
+  });
+  
+  if (locationErrorPresent) newErrors.push('Invalid Location');
+
+  return newErrors;
+
+};
+
 const businessErrorsReducer = (state = [], action) => {
   Object.freeze(state);
 
   switch(action.type) {
     case RECEIVE_BUSINESS_ERRORS:
       console.log(action.errors);
-      return action.errors;
+      const sanitisedErrors = convertLocationErrors(action.errors);
+      console.log(sanitisedErrors);
+      return sanitisedErrors;
       
     case RECEIVE_GEOCODING_ERRORS:
       return ['Invalid Location'];
